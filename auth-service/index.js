90cffa6db50c2343fe2cb3d9fc418cf5523bd510
Abhijeet-Kadance <abhijeet.kadance.96@gmail.com>
2022-01-20 22:02:36 +0530

    create consumer and Publisher services and an authentication service

--- auth-service/index.js
+++ auth-service/index.js
@@ -2,27 +2,55 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT_ONE || 7070;
 const mongoose = require("mongoose")
-const user = require('./model/user')
+const User = require("./user");
+const jwt = require("jsonwebtoken");
 
-mongoose.connect('mongodb://localhost/auth-service',()=>{
-    console.log("Connected");
-});
-const db = mongoose.connection;
-db.on("error", (error) => {
-    console.log("Error: ", error)
-})
-
-db.once("open", ()=> {
-    console.log("open database")
-
-    user.insertMany({id: "dsjd", userName: "shivam"})
-
-})
-const MyModel = mongoose.model('Test', new mongoose.Schema({ name: String }));
-MyModel.findOne(function(error, result) { /* ... */ });
-
-
+mongoose.connect("mongodb://localhost/auth-service", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log(`Auth-service DB connected`)
+}
+);
 app.use(express.json());
+
+app.post("/auth/login", async(req,res)=>{
+    const {email,password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+        return res.json({message:"User dosen't exist" });
+    }else{
+        if(password !== user.password){
+            return res.json({message:"Password Incorrect"});
+        }
+        const payload = {
+            email,
+            name:user.name
+        };
+        jwt.sign(payload,"secret",(err,token)=>{
+            if(err) console.log(err);
+            else{
+                return res.json({ token: token});
+            }
+        })
+    }
+})
+
+app.post("/auth/register", async (req,res) => {
+    const {email,password,name} = req.body;
+    const userExists = await User.findOne({ email });
+    if(userExists){
+        return res.json({message:"User already exixts"});
+    }else{
+        const newUser = new User({
+            name,
+            email,
+            password,
+        });
+        newUser.save();
+        return res.json(newUser);
+    }
+})
 app.listen(PORT,()=>{
     console.log(`Auth-service runing at ${PORT}`);
 })
